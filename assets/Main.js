cc519852461b0c6cdb32cb39f1666dad5598f69b
Khrouhman <acpvg@yahoo.com>
2021-10-24 18:48:08 -0700

    remove excess code
    -removed return rand num - added var instead
    -removed redundant : numclicks, append btn1,
    -change capitalize to string splice
    -head variable now edits text correctly

--- assets/Main.js
+++ assets/Main.js
@@ -1,21 +1,18 @@
-
 //Use button 'NameGen' to create new random name
 let btn1 = document.getElementById('NameGen');
-let head = document.getElementById("headTitle").innerHTML
-//let numClicks = 0;
+let head = document.getElementById("headTitle")
+
 btn1.innerHTML = "Don't click" ;
 btn1.onclick = function() {
 	
 	btn1.innerHTML = "Nah, I want a different name";
-	head = "Random Name: ";
+	head.innerHTML = "Random Name: ";
 	document.getElementById("Random Name").innerHTML = randomName();
-	//numClicks++;
 	
 }
-document.body.appendChild(btn1);
 
-//convert to ascii lowercase
-function toAlphabet(num){
+//Convert to ascii lowercase
+function toAlphabet(num) {
 	if (num < 1 || num > 26 || typeof num !=='number') {
 		alert(num);
 		return -1;
@@ -23,20 +20,16 @@ function toAlphabet(num){
 	return String.fromCharCode(num + 96);
 }
 
-//Random number 1-26
-function returnRandLet() {
-	return Math.round(Math.random() * 25 + 1);
-}
-
 //Random name with 7 letters constant vowel : CVVCCVC
 function randomName() {
 	var word = ""
 	for(let i = 0; i < 7; i++){
-		var letter = returnRandLet();
-		if(i == 0) {
-			word+= toAlphabet(letter).toUpperCase();
-		}
-		else if(i == 1 || i == 2 || i == 5) {	
+
+		//Grab random letter
+		var letter = Math.round(Math.random() * 25 + 1);
+
+		//if vowel add, if constant go back one
+		if(i == 1 || i == 2 || i == 5) {	
 			switch(letter) {
 				case 1:
 				case 5:
@@ -51,7 +44,9 @@ function randomName() {
 				break;
 			}
 		}
-		else {switch(letter) {
+		//if constant add, if constant go back one
+		else {
+			switch(letter) {
 				case 1:
 				case 5:
 				case 9:
@@ -66,5 +61,7 @@ function randomName() {
 			
 		}
 	}
-	return word;
+
+	//return capitalized word
+	return word.charAt(0).toUpperCase() + word.slice(1);;
 }
